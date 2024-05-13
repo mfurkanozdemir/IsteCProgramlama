beddc21ae86ff8104cbf78a6722c6111b20d51ec
Muhammed Furkan <122174098+mfurkanozdemir@users.noreply.github.com>
2024-05-13 21:47:30 +0300

    Add files via upload

---
+++ klavyedenSayiOkuma.c
@@ -0,0 +1,9 @@
+// Klavyeden 3 tam sayý okuyup ilk ikisinin toplamýný üçüncüsüne bölen ve sonucu uygun bir þekilde
+// ekranda gösteren program
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello World");
+	return 0;
+}
